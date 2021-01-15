32710eda72396c443a98cd117b127a7ae864df81
laurenstanton7791 <65672429+LaurenStanton7791@users.noreply.github.com>
2021-01-14 17:28:44 -0800

    js

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const btn_reset = document.getElementsByClassName('btn__reset')[0];
 let missed = 0; 
 
 const phrases = [
-  ['Alice in Wonderland'],
-  ['I Travel the World and the Seven Seas'],
-  ['Along Came a Spider'],
-  ['Green Eggs and Ham'], 
-  ['The Call of the Wild']
+  'Alice in Wonderland',
+  'I Travel the World and the Seven Seas',
+  'Along Came a Spider',
+  'Green Eggs and Ham', 
+  'The Call of the Wild'
 ];
 
-btn_reset.addEventListener('click', () => { // Hides button when clicking Start Game
+btn_reset.addEventListener('click', () => { // Hides button to start the game
   const startGame = document.getElementById('overlay');
   startGame.style.display = 'none';  
 });
@@ -21,116 +21,69 @@ function getRandomPhraseAsArray(arr) {  // does random stuff to arrays that is p
   return arr[randomNumber];
   };
 
-  
-function addPhraseToDisplay(arr) {
-  for(let i = 0; i < arr.length; i++) { // 
-      let li = document.createElement('li'); // create 'li'
-      li.textContent = arr[i]; // add content to 'li[i]'
-      let ul = phrase.querySelector('ul'); // declare ul
-      ul.appendChild(li); // attach 'li' with content to 'ul'
-      if( arr[i] != ' ' ){ // test for character or white space
-          li.classList.add('letter'); // add class letter to character
-      }
-      else {
-          li.classList.add('space'); // add class space to character
-      }
+function addPhraseToDisplay(arr)  {
+  for (let i = 0; i < arr.length; i++) {
+    let li = document.createElement('li');
+    li.textContent = arr[i];
+    let ul = phrase.querySelector('ul');
+    ul.appendChild(li);
+    if( arr[i] != '' ) {
+      li.classList.add('letter');
+    } else { 
+      li.classList.add('space');
+    }
   }
 };
 
-
-<<<<<<< HEAD
-=======
-
-function addPhraseToDisplay(arr) { // do stuff any arr that is passed in, and add to `#phrase ul`
- 
-}
-
->>>>>>> 55d132772d0c9f7cd7fb8abbc74fee335826058d
 const phraseArray = getRandomPhraseAsArray(phrases);
 // addPhraseToDisplay(phraseArray);
 addPhraseToDisplay(phraseArray.toLowerCase());
 
-<<<<<<< HEAD
-/*function checkLetter () => {
-=======
-function checkLetter () {
->>>>>>> 55d132772d0c9f7cd7fb8abbc74fee335826058d
 
-}
-function checkWin  () {
-    let show = document.querySelectorAll('.show');
-    let letters = document.querySelectorAll('.letter');
-    let title = overlay.querySelector('.title');
-    if (show.length == letters.length) {
-      btn_reset.style.display = 'none';
-      overlay.className = 'win';
-      title.textContent = 'win';
-      overlay.style.display = 'flex';
-    } else if (missed >= 5) {
-      btn_reset.style.display = 'none';
-      overlay.className = 'lose';
-      title.textContent = 'lose';
-      overlay.style.display = 'flex';
-    }
+function checkLetter(btn) { 
+  let lis = phrase.getElementsByTagName('li'); 
+  let match = null;
+
+  for(let i = 0; i < lis.length; i++) {
+      if(btn === lis[i].textContent) {
+          lis[i].classList.add('show');
+          match = btn;
+          
+      }
   }
+  return match;
+};
 
-qwerty.addEventListener('click', () => {
-
-  });
-/*const myButton = document.getElementsById('myButton');
-const correct = [];
-const incorrect = []; 
-const missedAnswers = 0;
-const qwerty = '';
-const phrases = [
-    ['alice in wonderland'],
-    ['i travel the world and the seven seas'],
-    ['along came a spider'],
-    ['green eggs and ham'], 
-    ['the call of the wild']
- ];
-
-myButton.addEventListener('click', () => {
-          startDiv.style.display = 'none';                 
+qwerty.addEventListener('click', (event) => {
+  let keyrow = qwerty.querySelectorAll('.keyrow');
+  let button = qwerty.getElementsByTagName('button');
+  if ( event.target.tagName === 'BUTTON' && button.classList != 'chosen' ) {
+      event.target.classList.add('chosen');
+      event.target.disabled = true;
+      const letterFound = checkLetter(event.target.textContent);
+      if (letterFound == null) {
+          let ol = document.querySelectorAll('#scoreboard ol li img');
+          ol[missed].src = "images/lostHeart.png";
+          missed += 1;
+      }
+  }
+  checkWin();
 });
-
-
-
-
-for ( let i = 0; i < questions.length; i++ ) {
-    let question = questions[i][0];
-    let answer = questions[i][1];
-    let response = prompt(question);
-    
-    if ( response === answer ) {
-      correctAnswers++;
-      correct.push(question);
-    } else {
-      incorrect.push(question);
-    }
+function checkWin() {
+  let letter = document.querySelectorAll(".letter"); // declare variable
+  let show = document.querySelectorAll(".show"); // declare variable
+  const overlay = document.getElementById('overlay'); // reuse variable
+  if ( show.length == letter.length ) {
+      overlay.style.display = 'block';
+      // overlay.className = 'win';
+      overlay.classList.add("win")
+      overlay.textContent = 'You win!';
   }
-  
-  function createListItems(arr) {
-    let items = '';
-    for (let i = 0; i < arr.length; i++) {
-      items += `<li>${arr[i]}</li>`;
-    }
-    return items;
+  if ( missed >= 5 ) {
+      overlay.style.display = 'block';
+      // overlay.className = 'lose';
+      overlay.classList.add('lose');
+      overlay.textContent = 'You lose'; // Dsiplay message you lose 
   }
-  
-  const btn_reset 
-  
-  // 4. Display the number of correct answers to the user
-  
-  let html = `
-    <h1>You got ${correctAnswers} question(s) correct</h1>
-    <h2>You got these questions right:</h2>
-    <ol>${ createListItems(correct) }</ol>
-  
-    <h2>You got these questions wrong:</h2>
-    <ol>${ createListItems(incorrect) }</ol>
-  `;
-  
-  document.querySelector('main').innerHTML = html;*/
-
+};
 
